93adae7e837a63cc27932731cae8a54415fe327c
Cole Reynolds <cpjreynolds@gmail.com>
2017-01-16 12:41:33 -0500

    some rough benches

--- src/test.rs
+++ src/test.rs
@@ -1,18 +1,16 @@
 extern crate typed_arena;
+extern crate test;
+extern crate rand;
 
 use std::cell::Cell;
 use std::mem;
 
+use self::typed_arena::Arena;
+use self::test::Bencher;
+use self::rand::Rand;
+
 use super::{Reap, Rp};
 
-struct DropTracker<'a>(&'a Cell<usize>);
-impl<'a> Drop for DropTracker<'a> {
-    fn drop(&mut self) {
-        self.0.set(self.0.get() + 1);
-    }
-}
-
-struct Node<'a>(Option<Rp<Node<'a>>>, usize, DropTracker<'a>);
 
 // Simple convenience function for the number of chunks in the given `Reap`.
 fn n_chunks<T>(reap: &Reap<T>) -> usize {
@@ -21,6 +19,15 @@ fn n_chunks<T>(reap: &Reap<T>) -> usize {
 
 #[test]
 fn reap_as_intended() {
+    struct DropTracker<'a>(&'a Cell<usize>);
+    impl<'a> Drop for DropTracker<'a> {
+        fn drop(&mut self) {
+            self.0.set(self.0.get() + 1);
+        }
+    }
+
+    struct Node<'a>(Option<Rp<Node<'a>>>, usize, DropTracker<'a>);
+
     let drop_counter = Cell::new(0);
     let reap = Reap::with_capacity(2);
 
@@ -150,10 +157,10 @@ fn test_zero_cap() {
     assert_eq!(*b, 2);
 }
 
-struct ZeroSized;
-
 #[test]
 fn test_zero_sized_type() {
+    struct ZeroSized;
+
     let reap = Reap::new();
     let mut v = Vec::with_capacity(100);
 
@@ -162,4 +169,81 @@ fn test_zero_sized_type() {
         v.push(alloc);
     }
     assert_eq!(n_chunks(&reap), 0);
+}
+
+// Before you look at these benchmarks, please be advised that I have absolutely zero experience
+// writing benchmarks, and the following are just my best effort.
+//
+// That being said, if someone with more experience could help improve these that would be
+// wonderful.
+
+#[bench]
+fn bench_reap_copy(b: &mut Bencher) {
+    const CAP: usize = 8192;
+
+    let reap = Reap::new();
+    let mut storage = Vec::with_capacity(CAP);
+
+    b.iter(|| {
+        for i in 0..CAP {
+            let alloc = reap.allocate(i);
+            storage.push(alloc);
+        }
+
+        for i in (0..(CAP - (CAP / 4))).filter(|n| n % 4 == 0) {
+            storage.swap_remove(i);
+        }
+
+        for i in 0..(CAP / 4) {
+            let alloc = reap.allocate(i);
+            storage.push(alloc);
+        }
+    });
+}
+
+#[bench]
+fn bench_arena_copy(b: &mut Bencher) {
+    const CAP: usize = 8192;
+
+    let arena = Arena::new();
+    let mut storage = Vec::with_capacity(CAP);
+
+    b.iter(|| {
+        for i in 0..CAP {
+            let alloc = arena.alloc(i);
+            storage.push(alloc);
+        }
+
+        for i in (0..(CAP - (CAP / 4))).filter(|n| n % 4 == 0) {
+            storage.swap_remove(i);
+        }
+
+        for i in 0..(CAP / 4) {
+            let alloc = arena.alloc(i);
+            storage.push(alloc);
+        }
+    });
+}
+
+#[bench]
+fn bench_box_copy(b: &mut Bencher) {
+    const CAP: usize = 8192;
+
+    let mut storage = Vec::with_capacity(CAP);
+
+    b.iter(|| {
+        for i in 0..CAP {
+            let alloc = Box::new(i);
+            storage.push(alloc);
+        }
+
+        for i in (0..(CAP - (CAP / 4))).filter(|n| n % 4 == 0) {
+            storage.swap_remove(i);
+        }
+
+        for i in 0..(CAP / 4) {
+            let alloc = Box::new(i);
+            storage.push(alloc);
+        }
+    });
 }
